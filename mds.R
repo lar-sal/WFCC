1c6fad2752db575c8a3e2a07e191bdcf4d759b6b
Belén García Pascual <114729515+BelenGarciaPascual@users.noreply.github.com>
2024-01-04 17:11:57 +0100

    Update mds.R

--- mds.R
+++ mds.R
@@ -1,48 +1,48 @@
 library(ggplot2)
 #Synthetic hypercubes for fluxes
-df = read.csv("Distance_matrix_flux.csv")
+df = read.csv("./Data_files_hypercubes/Distance_matrix_flux.csv")
 dist_matrix = as.matrix(df[1:nrow(df), 2:ncol(df)])
 mds_result <- cmdscale(dist_matrix, k = 2)
 mds.df = data.frame(label=colnames(df)[2:ncol(df)],
                     x = mds_result[,1],
                     y = mds_result[,2])
-svg("Outputs_github/MDS_flux.svg")
+svg("./Data_files_hypercubes/Outputs_github/MDS_flux.svg")
 ggplot(mds.df, aes(x=x,y=y,label=label)) + geom_point() + geom_text(nudge_y=-0.05) + 
   xlab("MDS coord 1") + ylab("MDS coord 2") + theme_light()
 dev.off()
 
 #Synthetic hypercubes for probabilities
-df = read.csv("Distance_matrix_probability.csv")
+df = read.csv("./Data_files_hypercubes/Distance_matrix_probability.csv")
 dist_matrix = as.matrix(df[1:nrow(df), 2:ncol(df)])
 mds_result <- cmdscale(dist_matrix, k = 2)
 mds.df = data.frame(label=colnames(df)[2:ncol(df)],
                     x = mds_result[,1],
                     y = mds_result[,2])
-svg("Outputs_github/MDS_probability.svg")
+svg("./Data_files_hypercubes/Outputs_github/MDS_probability.svg")
 ggplot(mds.df, aes(x=x,y=y,label=label)) + geom_point() + geom_text(nudge_y=-0.05) + 
   xlab("MDS coord 1") + ylab("MDS coord 2") + theme_light()
 dev.off()
 
 #Klebsiella for fluxes
-df = read.csv("distance_matrix_kelbsiella_f.csv")
+df = read.csv("./Data_files_hypercubes/distance_matrix_kelbsiella_f.csv")
 dist_matrix = as.matrix(df[1:nrow(df), 2:ncol(df)])
 mds_result <- cmdscale(dist_matrix, k = 2)
 mds.df = data.frame(label=colnames(df)[2:ncol(df)],
                     x = mds_result[,1],
                     y = mds_result[,2])
-svg("Outputs_github/klebsiella_f_MDS.svg")
+svg("./Data_files_hypercubes/Outputs_github/klebsiella_f_MDS.svg")
 ggplot(mds.df, aes(x=x,y=y,label=label)) + geom_point() + geom_text(nudge_y=-0.05) + 
   xlab("MDS coord 1") + ylab("MDS coord 2") + theme_light()
 dev.off()
 
 #Klebsiella for probabilities
-df = read.csv("distance_matrix_kelbsiella_p.csv")
+df = read.csv("./Data_files_hypercubes/distance_matrix_kelbsiella_p.csv")
 dist_matrix = as.matrix(df[1:nrow(df), 2:ncol(df)])
 mds_result <- cmdscale(dist_matrix, k = 2)
 mds.df = data.frame(label=colnames(df)[2:ncol(df)],
                     x = mds_result[,1],
                     y = mds_result[,2])
-svg("Outputs_github/klebsiella_p_MDS.svg")
+svg("./Data_files_hypercubes/Outputs_github/klebsiella_p_MDS.svg")
 ggplot(mds.df, aes(x=x,y=y,label=label)) + geom_point() + geom_text(nudge_y=-0.05) + 
   xlab("MDS coord 1") + ylab("MDS coord 2") + theme_light()
 dev.off()
